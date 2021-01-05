1bc10c81fd677eadf9ce026e3f4e5af238e00dd0
Romy Greenfield <rgreenfield@adaptavist.com>
2021-01-05 21:23:12 +0000

    refactored some folder structure and added notes

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const postProfileController = require('./controllers/profile/postProfile');
-const getOffersController = require('./controllers/offer/getOffers');
+
+// Import controller functions (these are always the last piece of middleware)
+const postProfileController = require('./controllers/postProfile');
+const getOffersController = require('./controllers/getOffers');
+
+// Import custom middleware functions 
 const authorization = require('./middlewares/authentication');
-const parameterCheck = require('./middlewares/parameterCheck');
+const parameterCheck = require('./middlewares/validation');
 const logging = require('./middlewares/logging');
+
 const app = express();
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// Examples of application level middleware
+app.use(express.json()); // express middleware for parsing application/json
+app.use(express.urlencoded({ extended: true })); // express middleware for parsing application/x-www-form-urlencoded
 app.use(logging); // custom middleware we have created for this demo 
 
 const profileMiddlewares = [parameterCheck, authorization];
 
+// Examples of route level middleware
 app.post('/profile', profileMiddlewares, postProfileController);
 app.get('/offers', getOffersController);
 
