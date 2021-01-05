8a6152503686f438b12dc7642918d566a17f95d3
Romy Greenfield <rgreenfield@adaptavist.com>
2021-01-05 21:16:58 +0000

    updated nodemon and fixed vulnerabilities, added note for loggin at application level

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(logging);
+app.use(logging); // custom middleware we have created for this demo 
 
 const profileMiddlewares = [parameterCheck, authorization];
 
